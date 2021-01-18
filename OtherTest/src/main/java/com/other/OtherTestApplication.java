836d9e426a1aad3016b72ec59856a7a46ee4ee0c
JWMA <butterprc20@nate.com>
2021-01-18 22:24:22 +0900

    초기화

---
+++ OtherTest/src/main/java/com/other/OtherTestApplication.java
@@ -0,0 +1,13 @@
+package com.other;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtherTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtherTestApplication.class, args);
+	}
+
+}
---
+++ OtherTest/src/test/java/com/other/OtherTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.other;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtherTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
